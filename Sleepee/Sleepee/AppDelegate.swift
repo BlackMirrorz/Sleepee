0af7086f59554fe97b06b11cc545e21e734bcdac
Josh Robbins <joshrobbins1981@me.com>
2022-04-15 19:39:39 +0900

    Initiali Setup
    add(SwiftLint)
    add(Nuke)
    add(PanModel)
    add(MarqueLabel)

---
+++ Sleepee/Sleepee/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Sleepee
+//
+//  Created by Josh Robbins on 2022/04/15.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    return true
+  }
+  
+  // MARK: UISceneSession Lifecycle
+  
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+  
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
+}
---
+++ Sleepee/Sleepee/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Sleepee
+//
+//  Created by Josh Robbins on 2022/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
